1ccb9658b55d07796ab9f093d4f2dcfc1301035e
yousrakdc <yousrakdc@gmail.com>
2024-04-09 15:12:07 +0000

    ascending order

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,6 +5,7 @@
 /**
  * insertion_sort_list - sorts a doubly linked list of integers
  * in ascending order
+ * using the Insertion sort algorithm
  * @list: double pointer to the head of the list
  */
 
