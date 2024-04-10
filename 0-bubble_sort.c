5fd3fcd44238da40e6d204a92a54e9c632ecbc53
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-04-10 11:17:27 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,9 +17,9 @@ void bubble_sort(int *array, size_t size)
 	if (!array || !size)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
